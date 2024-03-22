c65869544ea2e5d4449c6ee57a3ae14c088be54f
Praneeth-Kattamuri <kattamuri.k@northeastern.edu>
2024-03-21 21:04:40 -0400

    Final changes to views

--- views.sql
+++ views.sql
@@ -1,8 +1,12 @@
+-- VIEWS START FROM HERE 
+
 -- Category list
 
 CREATE OR REPLACE VIEW category_view AS
 SELECT id, name
 FROM category;
+
+
 
 -- store list
 
@@ -12,6 +16,7 @@ FROM return r
 JOIN order_product op ON r.order_product_id = op.id
 JOIN product p ON op.product_id = p.id;
 
+
 -- Store Rating
 
 CREATE OR REPLACE VIEW store_average_rating_view AS
@@ -20,46 +25,44 @@ SELECT store_id,
 FROM feedback
 GROUP BY store_id;
 
+
 -- Price Charged
 
 CREATE OR REPLACE VIEW product_discount_association AS
-SELECT p.id AS product_id,
+SELECT distinct p.id AS product_id,
        p.category_id,
        p.price,
        NVL(d.discount_rate, 0) AS discount_rate
 FROM product p
-LEFT JOIN discount d ON p.category_id = d.category_id
-LEFT JOIN customer_order o ON p.id = o.id
-                      AND o.order_date BETWEEN d.start_date AND d.end_date;
-
- 
+ JOIN discount d ON p.category_id = d.category_id
+ JOIN order_product op ON p.id = op.product_id
+                       JOIN customer_order o ON op.customer_order_id = o.id where o.order_date BETWEEN d.start_date AND d.end_date;
+                      
+ -- view for per unit product
 CREATE OR REPLACE VIEW order_product_actual_price_per_unit AS
 SELECT op.id AS order_product_id,
        op.customer_order_id,
        op.product_id,
        op.quantity,
        CASE
-           WHEN pda.discount_rate > 0 THEN (pda.price - (pda.price * pda.discount_rate / 100)) / op.quantity
+           WHEN pda.discount_rate > 0 THEN (pda.price - (pda.price * pda.discount_rate / 100))
            ELSE pda.price
-       END AS price_per_unit
+       END AS price_charged
 FROM order_product op
 JOIN customer_order o ON op.customer_order_id = o.id
 JOIN product_discount_association pda ON op.product_id = pda.product_id;
 
-CREATE OR REPLACE VIEW order_total_price_ AS
+
+-- total price for all units
+CREATE OR REPLACE VIEW order_total_price_per_unit AS
 SELECT customer_order_id,
-       SUM(price_per_unit * quantity) AS total_price
+       SUM(price_charged * quantity) AS total_price
 FROM order_product_actual_price_per_unit
 GROUP BY customer_order_id;
-
 
 -- Refund Amount
 
 CREATE OR REPLACE VIEW refund_amount_view AS
-SELECT CASE 
-           WHEN seller_refund = 1 THEN (SELECT op.price_per_unit * r.quantity_returned - NVL(r.processing_fee, 0)
-                                       FROM order_product_actual_price_per_unit op
-                                       WHERE op.order_product_id = r.order_product_id)
-           ELSE 0
-       END AS refund_amount
-FROM return r;
+SELECT distinct op.customer_order_id,op.product_id,(op.price_charged * r1.quantity_returned) - NVL(r1.processing_fee, 0) as refund_amount
+                                       FROM order_product_actual_price_per_unit op join return r1
+                                       on op.order_product_id = r1.order_product_id where r1.seller_refund > 0 ;
